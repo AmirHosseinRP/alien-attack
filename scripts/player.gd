897778715a7bcde61993b3890e5495e058d04679
a.rezapanah <a.rezapanah@dotin.ir>
2026-09-13 16:52:58 +0330

    game and player scene and simple player movements implemented

---
+++ scripts/player.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export_category("Constants")
+@export var speed = 300
+
+
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2(0, 0)
+
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+
+	move_and_slide()
